6c6c370e43a5494c9b67fd11c787a363da9ff29e
ch112f <c112181135@nkust.edu.tw>
2026-03-12 04:01:55 +0000

    123

---
+++ Q2_Fish.java
@@ -0,0 +1,16 @@
+public class Q2_Fish{
+    String name;
+    double weight;
+
+    public void displayinfo(){
+        System.out.println("這隻魚的名字是:"+name+"重量是:"+weight+"公斤");
+    }
+    public void main(String[] args){
+        Q2_Fish myFish=new Q2_Fish();
+        myFish.name="鮭魚";
+        myFish.weight=2.5;
+        myFish.displayinfo();
+    }
+
+
+}
